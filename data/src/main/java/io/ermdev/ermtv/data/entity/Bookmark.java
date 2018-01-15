89a17fefce691f41a9dcc66b3382000651a187c3
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-15 22:02:54 +0800

    Create a bookmark class.

---
+++ data/src/main/java/io/ermdev/ermtv/data/entity/Bookmark.java
@@ -0,0 +1,13 @@
+package io.ermdev.ermtv.data.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tblboomark")
+public class Bookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Item item;
+}
