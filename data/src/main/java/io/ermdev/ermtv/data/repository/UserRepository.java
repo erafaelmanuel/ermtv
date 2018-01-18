bfa0b7d134815efe60c27f420fe2d595ff30ef14
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-19 01:42:36 +0800

    Add findById method in UserRepository."

--- data/src/main/java/io/ermdev/ermtv/data/repository/UserRepository.java
+++ data/src/main/java/io/ermdev/ermtv/data/repository/UserRepository.java
@@ -2,6 +2,11 @@ package io.ermdev.ermtv.data.repository;
 
 import io.ermdev.ermtv.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("from User where id=:userId")
+    User findById(@Param("userId") Long userId);
 }
