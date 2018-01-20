b3291a57baf06b0b4e3bba8f9f5db7e288e2692d
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-20 14:13:24 +0800

    Create a new method for item repository.

--- data/src/main/java/io/ermdev/ermtv/data/repository/ItemRepository.java
+++ data/src/main/java/io/ermdev/ermtv/data/repository/ItemRepository.java
@@ -2,6 +2,16 @@ package io.ermdev.ermtv.data.repository;
 
 import io.ermdev.ermtv.data.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Query("from Item where id=:itemId")
+    Item findById(@Param("userId") Long itemId);
+
+    @Query("from Item")
+    List<Item> findAll();
 }
