64fb2c8255160dcf6c733d99cc03642bc177a150
erafaelmanuel <erafaelmanuel@gmail.com>
2018-01-26 03:29:13 +0800

    Create an user cotroller

---
+++ data/src/main/java/io/ermdev/ermtv/data/controller/UserController.java
@@ -0,0 +1,9 @@
+package io.ermdev.ermtv.data.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+}
