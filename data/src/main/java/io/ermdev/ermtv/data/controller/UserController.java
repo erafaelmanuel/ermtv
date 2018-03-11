e40a2314d1db3bc2716784327c6925f26e58f5bd
erafaelmanuel <erafaelmanuel@gmail.com>
2018-03-11 20:44:58 +0800

    Reset

--- data/src/main/java/io/ermdev/ermtv/data/controller/UserController.java
+++
@@ -1,9 +0,0 @@
-package io.ermdev.ermtv.data.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("user")
-public class UserController {
-}
